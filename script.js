e37f81b6411bf528d9fbd05c66dc3a45b335a280
Morten Gross <mg@vuffeli.dk>
2021-02-05 13:38:52 +0100

    Now adds two numbers together from the input fields

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 "use strict";
 
+let value1;
+let value2;
+
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
     console.log("start");
 
-
     document.querySelector("#calculate").addEventListener("click", calculateResults);
-    document.querySelector("#clear").addEventListener("click", clearResults);
+    clearButton.addEventListener("click", clearResults);
 }
 
 function calculateResults() {
     console.log("calculateResults");
+    const firstNumber = document.querySelector("#firstnumber");
+    const secondNumber = document.querySelector("#secondnumber");
+
+    value1 = firstNumber.value;
+    value2 = secondNumber.value;
+    console.log(Number(value1) + Number(value2));
+
 }
 
 function clearResults() {
