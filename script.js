8de9db7f224df5d508806bb17db6afce3401a8d5
Morten Gross <mg@vuffeli.dk>
2021-02-05 17:55:04 +0100

    World's worst calculator is actually working

--- script.js
+++ script.js
@@ -1,42 +1,81 @@
 "use strict";
+
+// We need this in multiple functions
 const results = document.querySelector("#results");
 
+// Setting up the world's worst calculator
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
     console.log("start");
 
+    // There is already a couple of li tags, we need to get rid of those
     results.innerHTML = "";
 
+    // We need to make these clickable
     document.querySelector("#calculate").addEventListener("click", calculateResults);
     document.querySelector("#clear").addEventListener("click", clearResults);
 }
 
+// We clicked on #calculate
 function calculateResults() {
     console.log("calculateResults");
+
+    // Read first number
     const firstNumber = document.querySelector("#firstnumber");
+    let value1 = firstNumber.value;
+    console.log(value1);
+
+    // Read second number 
     const secondNumber = document.querySelector("#secondnumber");
+    let value2 = secondNumber.value;
+    console.log(value2);
+
+    // Read operator
     const operator = document.querySelector("#operator");
     let selectedOperator = operator.value;
+    console.log(selectedOperator);
 
-    let value1 = firstNumber.value;
-    let value2 = secondNumber.value;
-
+    // Now we're actually gonna do the calculation
+    let result;
     if (selectedOperator === "add") {
-        firstNumber.value = Number(value1) + Number(value2);
+        result = Number(value1) + Number(value2);
+        console.log(result);
     } else if (selectedOperator === "sub") {
-        firstNumber.value = Number(value1) - Number(value2);
+        result = Number(value1) - Number(value2);
+        console.log(result);
     } else if (selectedOperator === "mul") {
-        firstNumber.value = Number(value1) * Number(value2);
+        result = Number(value1) * Number(value2);
+        console.log(result);
     } else if (selectedOperator === "div") {
-        firstNumber.value = Number(value1) / Number(value2);
+        result = Number(value1) / Number(value2);
+        console.log(result);
     }
-    secondNumber.value = "";
 
-    let result = document.createElement("li");
-    result.textContent = firstNumber.value;
-    results.appendChild(result);
+    // Do we have to round the result?
+    const doRound = document.querySelector("#doround");
+    console.log(doRound.checked); 
 
+    // And if do have to round, how many decimals do we need? 
+    const decimals = document.querySelector("#decimals");
+    let selectedDecimals = decimals.value;
+    console.log(selectedDecimals);
+
+    // Update the input fields with the result
+    if (doRound.checked === true) {
+        firstNumber.value = result.toFixed(selectedDecimals);
+        secondNumber.value = "";
+    } else {
+        firstNumber.value = result;
+        secondNumber.value = "";
+    }
+
+    // Append the result to the end of the results list
+    let resultList = document.createElement("li");
+    resultList.textContent = firstNumber.value;
+    results.appendChild(resultList);
+
+    // We're just gonna add some automatic scroll
     const scroll  = results.scrollHeight;
     results.scrollBy(0, scroll);
 }
